410206e3f007c1e1e4aa8f359700ba79132579cf
Kaushiki Kumari <kaushiki@cupidknot.com>
2022-12-16 16:25:30 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Supriya Assignment',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.indigo,
